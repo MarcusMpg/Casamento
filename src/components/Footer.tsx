ec9488cb33dd8e10df3ef52144c4565a967ba0be
MarcusMpg <marcus.ktkh@gmail.com>
2025-11-16 07:40:58 -0300

    add fotos e imgs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,12 +6,10 @@ const Footer = () => {
       <div className="container mx-auto max-w-6xl text-center">
         <div className="flex items-center justify-center gap-2 mb-4">
           <Heart className="w-6 h-6 text-primary fill-primary" />
-          <p className="text-2xl font-bold text-foreground">N & N</p>
+          <p className="text-2xl font-bold text-foreground">L & R</p>
           <Heart className="w-6 h-6 text-primary fill-primary" />
         </div>
-        <p className="text-muted-foreground mb-2">
-          24 de Janeiro de 2026
-        </p>
+        <p className="text-muted-foreground mb-2">24 de Janeiro de 2026</p>
         <p className="text-sm text-muted-foreground">
           Feito com amor para celebrar nosso para sempre
         </p>
