dacd5c68f5ab470cdbb421e054d90b69d12aff77
MarcusMpg <marcus.ktkh@gmail.com>
2025-11-27 17:24:24 -0300

    ajuste de pastas de img

--- src/components/DressCode.tsx
+++ src/components/DressCode.tsx
@@ -1,5 +1,5 @@
 import { Shirt, Sparkles } from "lucide-react";
-import padrinhosImg from "@/assets/dressCode/padrinho.png";
+import padrinhosImg from "@/assets/dressCode/padrinhos/padrinho.png";
 import convidados1 from "@/assets/dressCode/convidados01.png";
 import convidados2 from "@/assets/dressCode/convidados02.png";
 import convidados3 from "@/assets/dressCode/convidado03.png";
