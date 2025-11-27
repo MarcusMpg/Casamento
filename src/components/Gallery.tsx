ee9f33ed0bd50bef49273e1ea850fa8c392ff702
MarcusMpg <marcus.ktkh@gmail.com>
2025-11-27 17:04:24 -0300

    galeria com imgdesfocado no bg

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -78,8 +78,15 @@ const Gallery = () => {
             >
               <img
                 src={image.src}
+                alt=""
+                aria-hidden="true"
+                className="absolute inset-0 w-full h-full object-cover blur-lg scale-110"
+              />
+
+              <img
+                src={image.src}
                 alt={image.alt}
-                className="w-full h-80 object-contain bg-black group-hover:scale-105 transition-transform duration-500"
+                className="relative w-full h-80 object-contain"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-foreground/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
             </div>
